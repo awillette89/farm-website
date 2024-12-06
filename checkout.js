59608c258ec5c8007d7849498824fcb33ed077bc
awillette89 <awillette@gmail.com>
2024-12-06 14:57:43 -0500

    Changes

--- checkout.js
+++ checkout.js
@@ -1,4 +1,4 @@
-const stripe = Stripe('pk_test_51QS0tQGMpgCaGok4YWtuf7yRAggwkWtxYDRUKpjTsh3smR0RxxzU1QSYyjdljCsklqzl8AbsYY5anKAc11XQS3lS00mUTR7Z6w');
+const stripe = Stripe('');
 
 const appearance = { /* appearance */ };
 const options = {
