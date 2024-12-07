95887d2fad1bc82e73b20d815335c12266d44315
awillette89 <awillette@gmail.com>
2024-12-07 11:32:31 -0500

    Checkout editing.

--- checkout.js
+++ checkout.js
@@ -1,4 +1,6 @@
-const stripe = Stripe('');
+require('dotenv').config(); // Load environment variables
+
+const stripe = Stripe(process.env.STRIPE_PUBLIC_KEY); // Use the environment variable for the public key
 
 const appearance = { /* appearance */ };
 const options = {
@@ -7,6 +9,7 @@ const options = {
     defaultCollapsed: false,
   }
 };
+
 const elements = stripe.elements({ clientSecret, appearance });
 const paymentElement = elements.create('payment', options);
 paymentElement.mount('#payment-element');
